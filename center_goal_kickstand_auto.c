a632a0bc42f6a8e04424ed5030cf886905606b6d
Corey Porter <cp@conduce.com>
2015-02-13 11:01:03 -0800

    IR kickstand

--- center_goal_kickstand_auto.c
+++ center_goal_kickstand_auto.c
@@ -72,24 +72,14 @@ void setSpeeds (int left, int right) {
     motor[RIGHT_DRIVE] = left;
 }
 
-void setSpeedsCrawlAhead () { setSpeeds (80, 80); }
-void setSpeedsCrawlBack () { setSpeeds (-20, -20); }
-void setSpeedsSlowlyAhead () { setSpeeds (20, 20); }
-void setSpeedsSpiritedAhead () { setSpeeds (40, 40); }
-void setSpeedsALittleMoreAhead () { setSpeeds (50, 50); }
-void setSpeedsAllAheadFull () { setSpeeds (100, 100); }
-void setSpeedsLeftTurn () { setSpeeds (-0, 100); }
-void setSpeedsRightTurn () { setSpeeds (100, -0); }
-
-void setSpeedsLeftTurnSmall () { setSpeeds (0, 70); }
-void setSpeedsRightTurnSmall () { setSpeeds (70, 0); }
-
-void setSpeedsLeftPivot () { setSpeeds (-40, 40); }
-void setSpeedsRightPivot () { setSpeeds (40, -40); }
-
-void setSpeedsStop () { setSpeeds (0, 0); }
+void allStop () { setSpeeds (0, 0); }
 
 void sleep (float seconds) { wait10Msec (seconds * 100.0); }
+
+void driveFor (int left, int right, float time) {
+    setSpeeds (left, right);
+    sleep (time);
+}
 
 const int LEFT_SENSOR_CENTER = 5;
 const int RIGHT_SENSOR_CENTER = 5;
@@ -106,7 +96,7 @@ void dumpBallsInCenterGoal () {
     motor[LIFT] = 0;
 
     writeDebugStreamLine ("Dumpin the game elements");
-    servo[BALL_DUMP] = 160;
+    servo[BALL_DUMP] = 140;
     sleep (SERVO_DUMP_DURATION);
     writeDebugStreamLine ("Returning servo to start position");
     servo[BALL_DUMP] = 0;
@@ -118,29 +108,19 @@ void dumpBallsInCenterGoal () {
     motor[LIFT] = 0;
 }
 
-void backUntilTwoFives () {
-    writeDebugStreamLine ("Position One: Forward Until IR");
-    int iterations = 0;
-    const int MAX_ITERATIONS = 15;
-    setSpeedsCrawlBack ();
-    while (MAX_ITERATIONS > iterations++) {
-        int left_dir = irDirLeft ();
-        int right_dir = irDirRight ();
-        if (LEFT_SENSOR_CENTER == left_dir &&
-            RIGHT_SENSOR_CENTER == right_dir) {
-            break;
-        }
-        sleep (0.1);
-    }
+// Roughly straight-on
+void kickstandPositionOne () {
+    // Turn left
+    driveFor (100, -100, .25);
+    // Forward a bit
+    driveFor (50, 50, .25);
+    // Turn right towards the center element
+    driveFor (-100, 100, .5);
+    // Drive in, angling right, to knock it down.
+    driveFor (60, 100, 1.0);
 }
 
-void driveFor (int left, int right, float time) {
-    setSpeeds (left, right);
-    sleep (time);
-}
-
-void kickstandPositionOne () {}
-
+// Somewhat angled
 void kickstandPositionTwo () {
     // Angle slightly to the right
     driveFor (30, -60, .25);
@@ -150,6 +130,7 @@ void kickstandPositionTwo () {
     driveFor (60, 100, 1.0);
 }
 
+// Roughly straight-on
 void kickstandPositionThree () {
     // Turn left
     driveFor (100, -100, .25);
@@ -169,20 +150,29 @@ void kickstandIRExperimental () {
 
     // Now drive forward unti the IR beacon is on our left.
     setSpeeds (50, 50);
-    int iterations = 0;
-    int MAX_FWD_ITER = 20;
-    while (MAX_FWD_ITER > iterations++) {
-        int left_ir = irDirLeft ();
 
+    const int MAX_FWD_ITER = 20;
+    for (int i = 0; i < 20; ++i) {
+        int left_ir = irDirLeft ();
         if (1 >= left_ir) {
             break;
         }
         sleep (.05);
     }
 
-    // And now start running left in towards the goal.
-    driveFor (0, 100, .5);
-    // And ramming speed.
+    // Turn in unti we're angled towards the center element
+    setSpeeds (-50, 75);
+
+    const int MAX_ROT_ITER = 20;
+    for (int i = 0; i < MAX_ROT_ITER; ++i) {
+        int left_ir = irDirLeft ();
+        if (3 >= left_ir) {
+            break;
+        }
+        sleep (.05);
+    }
+
+    // Ramming speed!
     driveFor (100, 100, .5);
 }
 
@@ -205,7 +195,7 @@ void knockOverKickstand (int position) {
     kickstandIRExperimental ();
 #endif
 
-    setSpeedsStop ();
+    allStop ();
 }
 
 // Drive forward to the IR beacon. Return the number of times it senses
@@ -215,8 +205,7 @@ int guidedIRForward () {
     const int POWER_DIFFERENCE_THRESHOLD = 10;
     int zero_count = 0;
 
-    setSpeedsSpiritedAhead ();
-    sleep (0.1);
+    driveFor (40, 40, .1);
 
     int balance = 0;
     bool quit = false;
@@ -238,14 +227,14 @@ int guidedIRForward () {
             int diff = left - right;
             if (POWER_DIFFERENCE_THRESHOLD > abs (diff)) {
                 writeDebugStreamLine ("Guided IR: Simple Forward");
-                setSpeedsSlowlyAhead ();
+                setSpeeds (20, 20);
             } else if (left > right) {
                 writeDebugStreamLine ("Guided IR: Turn Left");
-                setSpeedsLeftTurnSmall ();
+                setSpeeds (0, 70);
                 ++balance;
             } else { // left < right
                 writeDebugStreamLine ("Guided IR: Turn Right");
-                setSpeedsRightTurnSmall ();
+                setSpeeds (70, 0);
                 --balance;
             }
         } else {
@@ -255,7 +244,7 @@ int guidedIRForward () {
     }
 
     writeDebugStreamLine ("Guided IR: Done!");
-    setSpeedsStop ();
+    allStop ();
 
     return balance;
 }
@@ -268,13 +257,11 @@ void driveToPositionOne () {
 
     // Get away from the wall
     writeDebugStreamLine ("Position One: Initial Forward");
-    setSpeedsSpiritedAhead ();
-    sleep (INITIAL_FORWARD_DURATION);
+    driveFor (40, 40, INITIAL_FORWARD_DURATION);
 
     // Turn left a bit
     writeDebugStreamLine ("Position One: Initial Left");
-    setSpeedsLeftPivot ();
-    sleep (INITIAL_LEFT_TURN_DURATION);
+    driveFor (-40, 40, INITIAL_LEFT_TURN_DURATION);
 
     // Drive forward until the IR beacon is at position 9 for the right
     // IR sensor
@@ -282,7 +269,7 @@ void driveToPositionOne () {
     writeDebugStreamLine ("Position One: Forward Until IR");
     int iterations = 0;
     const int MAX_FORWARD_ITERATIONS = 15;
-    setSpeedsSpiritedAhead ();
+    setSpeeds (40, 40);
     while (MAX_FORWARD_ITERATIONS > iterations++) {
         if (doneDrivingForwardTowardsPositionOne ()) {
             break;
@@ -295,7 +282,7 @@ void driveToPositionOne () {
 
     // Turn right until the IR beacon is at position 5 for both sensors
     // and the power difference between the two is minimal.
-    setSpeedsRightPivot ();
+    setSpeeds (40, -40);
     iterations = 0;
     const int MAX_ROTATE_ITERATIONS = 20;
     const int POWER_SAME_THRESHOLD = 20;
@@ -318,7 +305,7 @@ void driveToPositionOne () {
     }
 
     // Stop!
-    setSpeedsStop ();
+    allStop ();
     writeDebugStreamLine (
         "Position One: Finished turning right after %d iterations", iterations);
 }
@@ -339,7 +326,7 @@ int driveToCenterGoal () {
     int balance = guidedIRForward ();
     // setSpeeds(-50, -50);
     // sleep(0.4);
-    setSpeedsStop ();
+    allStop ();
 
     int position = 0;
     if (position_one) {
@@ -358,9 +345,8 @@ int driveToCenterGoal () {
 }
 
 task main () {
-    // initialize_robot ();
-
     // waitForStart();
+    initialize_robot ();
 
     int position = driveToCenterGoal ();
     dumpBallsInCenterGoal ();
